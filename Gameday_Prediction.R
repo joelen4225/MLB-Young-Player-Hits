06b785483e5d1e187f9060881d8409cba5eb7e21
Joe Leonard <64109094+joelen4225@users.noreply.github.com>
2023-08-10 13:14:37 -0300

    Changing the Gameday_Prediction.R File with New Model

--- Gameday_Prediction.R
+++ Gameday_Prediction.R
@@ -4,6 +4,24 @@
 install.packages("tidyverse")
 install.packages("dplyr")
 install.packages("baseballr")
+install.packages("caret")
+install.packages("randomForest")
+install.packages("readxl")
+
+# Loading the Libraries
+library(tidyverse)
+library(dplyr)
+library(baseballr)
+library(caret)
+library(randomForest)
+library(readxl)
+
+# This is Where I Input Data to Test the Day of the Game
+
+# Initializing the Packages
+install.packages("tidyverse")
+install.packages("dplyr")
+install.packages("baseballr")
 
 # Loading the Libraries
 library(tidyverse)
@@ -11,21 +29,19 @@ library(dplyr)
 library(baseballr)
 
 # Writing Down the Batter Statistics
-OBP <- .360
-BOP <- 3
-RE24 <- 0.43
-DFS.DK. <- 12.00
-DFS.FD. <- 15.70
-teams_home_league_record_pct <- .542
+BOP <- 9
+RE24 <- -0.81
+DFS.DK. <- 0.00
+DFS.FD. <- 0.00
 
 # Retrieving the Pitcher Statistics
-trial_run_db <- bref_daily_pitcher("2023-03-31", "2023-07-31")
-trial_run_db <- trial_run_db[trial_run_db$Name == "Alex Cobb", ]
-trial_run_db <- trial_run_db[, c(4, 7, 8, 14, 15, 29, 30, 33, 37)]
+trial_run_db <- bref_daily_pitcher("2023-03-31", "2023-08-09")
+trial_run_db <- trial_run_db[trial_run_db$Name == "Hunter Brown", ]
+trial_run_db <- trial_run_db[, c(7, 8, 29, 37)]
 
 # Merging the Batter and Pitcher Statistics
-left_join_db <- data.frame(OBP, BOP, RE24, DFS.DK., DFS.FD., teams_home_league_record_pct)
+left_join_db <- data.frame(OBP, BOP, RE24, DFS.DK., DFS.FD.)
 trial_run_db <- cross_join(trial_run_db, left_join_db)
 
 # Predicting the Number of Hits
-predict(hitsLinReg, newdata = trial_run_db)
+predict(result_rfe1, trial_run_db)
